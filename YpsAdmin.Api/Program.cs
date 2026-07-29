68030b293656a09317cb6f3e060263c068b554bb
Phone Win Tun <phonewinttun173@gmail.com>
2026-07-30 01:06:10 +0630

    chore: register domain services and PostGIS DbContext in FeatureManager

--- YpsAdmin.Api/Program.cs
+++ YpsAdmin.Api/Program.cs
@@ -1,17 +1,27 @@
+using Scalar.AspNetCore;
+using YpsAdmin.Domain.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Domain DbContext and Feature Services
+builder.AddDomain();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
     app.UseSwaggerUI();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithOpenApiRoutePattern("/swagger/{documentName}/swagger.json");
+    });
 }
 
 app.UseHttpsRedirection();
--- YpsAdmin.Domain/Features/FeatureManager.cs
+++ YpsAdmin.Domain/Features/FeatureManager.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using YpsAdmin.Database.AppDbContextModels;
+using YpsAdmin.Domain.Features.BusLine;
+using YpsAdmin.Domain.Features.BusStop;
+using YpsAdmin.Domain.Features.RouteStop;
+using YpsAdmin.Domain.Features.Store;
 
 namespace YpsAdmin.Domain.Features
 {
@@ -10,10 +14,17 @@ namespace YpsAdmin.Domain.Features
     {
         public static void AddDomain(this WebApplicationBuilder builder)
         {
+            // Register DbContext with PostgreSQL & NetTopologySuite PostGIS support
             builder.Services.AddDbContext<AppDbContext>(options =>
-                           options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    o => o.UseNetTopologySuite()));
 
-
-        }   
+            // Register Feature Services
+            builder.Services.AddScoped<IBusLineService, BusLineService>();
+            builder.Services.AddScoped<IBusStopService, BusStopService>();
+            builder.Services.AddScoped<IRouteStopService, RouteStopService>();
+            builder.Services.AddScoped<IYpsStoreService, YpsStoreService>();
+        }
     }
 }
