e630a75ae99aa4ae2fa998753ee583e3b74a7a04
Phone Win Tun <phonewinttun173@gmail.com>
2026-08-08 23:26:27 +0630

    feat(web): implement shadcn-inspired UI component primitives

---
+++ YpsAdmin.Web/Components/ButtonTypes.cs
@@ -0,0 +1,18 @@
+namespace YpsAdmin.Web.Components;
+
+public enum ButtonVariant
+{
+    Default,
+    Secondary,
+    Outline,
+    Ghost,
+    Destructive
+}
+
+public enum ButtonSize
+{
+    Sm,
+    Md,
+    Lg,
+    Icon
+}
